21f3acd8bcbee799acb78eb58de0db47b7d3c6e6
Mikael Malmsten <meamalxv@jyu.fi>
2024-11-05 14:28:31 +0200

    Kattavuus 100% mutta bugi koodissa enkä ollut varmaa saako sitä mennä sörkkimään

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,50 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisaaminen_kun_ei_tilaa(self):
+        self.varasto.lisaa_varastoon(12)
+        
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+    
+    def test_lisaataan_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-4)
+
+        #Vapaata tilaa ei pitäisi tulla lisää eli pysyy
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ota_varastosta_enemmän_kuin_tilaa(self):
+        saatu_maara = self.varasto.ota_varastosta(100)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+        self.varasto.lisaa_varastoon(6)
+
+        saatu_maara = self.varasto.ota_varastosta(100)
+
+        self.assertAlmostEqual(saatu_maara, 6)
+    
+    def test_ota_varastosta_neg(self):
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0)
+    
+    def test_stringina(self):
+        self.varasto.lisaa_varastoon(8)
+        self.assertEqual(str(self.varasto), "saldo = 8, vielä tilaa 2")
+
+    def test_konstruktori_alkusaldo_liikaa(self):
+        varasto = Varasto(10, 24)
+        self.assertAlmostEqual(varasto.paljonko_mahtuu(), 0)
+        self.assertAlmostEqual(varasto.tilavuus, 10)
+
+    def test_konstruktori_neg_tilavuus(self):
+        varasto = Varasto(-2, 0)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+        self.assertAlmostEqual(varasto.saldo, 0)
+        # Hajoaa, koska alku_saldo{0} < 0 == false,
+        # alku_saldo{0} <= tilavuus{-2} == false
+        # joten saldo = tilavuus{-2} (eikä self.tilavuus joka on nollattu)  
+
+    def test_konstruktori_alkusaldo_negatiivinen(self):
+        varasto = Varasto(10, -6)
+        self.assertAlmostEqual(varasto.paljonko_mahtuu(), 10)
+        self.assertAlmostEqual(varasto.tilavuus, 10)
